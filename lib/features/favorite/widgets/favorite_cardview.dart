68f4042b3ae21a1f8ed46c1b1f22084293a22ec3
windakurniaakmila <windakurniaakmila@gmail.com>
2025-01-02 00:19:52 +0700

    feat : layouting fav_card_widget ( coding )

--- lib/features/favorite/widgets/favorite_cardview.dart
+++ lib/features/favorite/widgets/favorite_cardview.dart
@@ -1,77 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
-import 'package:tubes2_uas_kelompok7/data/breeds/responsesmodel/breeds_responses_model.dart';
 
-class HomeCard extends StatelessWidget {
-  final BreedsResponsesModel image;
-  final VoidCallback onFavoritePressed;
-  final VoidCallback onLikePressed;
-  final double borderRadius;
+import 'package:tubes2_uas_kelompok7/data/favorite/responsesmodel/favorite_responses_model_get.dart';
 
-  const HomeCard({
-    super.key,
-    required this.image,
-    required this.onFavoritePressed,
-    required this.onLikePressed,
-    required this.borderRadius,
-  });
+class FavoriteCard extends StatelessWidget {
+  final FavouritesResponsesModelGet favorite;
+  final VoidCallback onDeleted;
+
+  const FavoriteCard(
+      {super.key, required this.favorite, required this.onDeleted});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-        color: Colors.blue,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(borderRadius),
+      color: Colors.brown,
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Favorite id : ${favorite.id}",
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                )),
+            const SizedBox(height: 10),
+            Text(
+              "Tanggal Favorite : ${favorite.createdAt}",
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+              ),
+            ),
+          ],
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                _buildTextSection(),
-                const SizedBox(height: 8),
-                _buildActionSection(),
-              ]),
-        ));
-  }
-
-  Widget _buildTextSection() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Nama Kucing : ${image.name}',
-          style: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-        const SizedBox(height: 10),
-        Text('Deskripsi : ${image.description}',
-            style: const TextStyle(fontWeight: FontWeight.normal))
-      ],
-    );
-  }
-
-  Widget _buildActionSection() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        IconButton(
-          icon: const Icon(
-            Icons.favorite,
-          ),
-          onPressed: onFavoritePressed,
-        ),
-        IconButton(
-          icon: const Icon(
-            Icons.thumb_up,
-            color: Colors.white,
-          ),
-          onPressed: onLikePressed,
-        ),
-      ],
+      ),
     );
   }
 }
